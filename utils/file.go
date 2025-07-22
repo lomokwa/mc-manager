f7995e77d5c4f583ba9f907dfa0c558e5f44f399
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2025-07-21 22:32:43 -0400

    fix: checking if folder exists before creating file

--- utils/file.go
+++ utils/file.go
@@ -1,9 +1,11 @@
 package utils
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 )
 
 func FileExists(filePath string) bool {
@@ -12,6 +14,12 @@ func FileExists(filePath string) bool {
 }
 
 func DownloadFile(url, filePath string) error {
+	dir := filepath.Dir(filePath)
+	err := os.MkdirAll(dir, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("failed to create directory %s: %w", dir, err)
+	}
+
 	resp, err := http.Get(url)
 	if err != nil {
 		return err
