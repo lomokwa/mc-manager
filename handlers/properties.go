8185a733c234b24c9beb8eceeaab0f3c8b93cff1
Lorenzo Mokwa <lorenzomokwa@outlook.com>
2026-06-29 23:09:25 -0400

    feat: added update server properties service

--- handlers/properties.go
+++ handlers/properties.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/lomokwa/mc-manager/services"
 	"github.com/lomokwa/mc-manager/types"
 )
 
@@ -11,4 +12,16 @@ func UpdateServerPropertiesHandler(c *gin.Context) {
 		c.JSON(400, types.APIResponse{Error: "invalid request body"})
 		return
 	}
+
+	if err := types.ValidateServerProperties(req.Properties); err != nil {
+		c.JSON(400, types.APIResponse{Error: err.Error()})
+	}
+
+	err := services.UpdateServerProperties(req.Properties)
+	if err != nil {
+		c.JSON(500, types.APIResponse{Error: "failed to update server properties"})
+		return
+	}
+
+	c.JSON(200, types.APIResponse{Success: true})
 }
